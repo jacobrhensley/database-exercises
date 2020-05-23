2aae7e4d11ba06aeae6b0674a3984c844a14a908
Jacob Hensley <jacobrhensley3@gmail.com>
2020-05-22 21:52:43 -0500

    created MYSQL for an albums table

---
+++ albums_migration.sql
@@ -0,0 +1,15 @@
+USE codeup_test_db;
+
+
+DROP TABLE IF EXISTS albums;
+
+
+CREATE TABLE albums (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist  VARCHAR(50) NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    release_date DATE,
+    sales INT UNSIGNED,
+    genre TEXT,
+    PRIMARY KEY (id)
+);
