4dd1eb206ae78d84ebc279840c23b53f4ca31faa
Jacob Hensley <jacobrhensley3@gmail.com>
2020-05-26 11:43:42 -0500

    updated table

--- albums_migration.sql
+++ albums_migration.sql
@@ -6,10 +6,10 @@ DROP TABLE IF EXISTS albums;
 
 CREATE TABLE albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    artist  VARCHAR(50) NOT NULL,
-    name VARCHAR(50) NOT NULL,
-    release_date DATE,
-    sales INT UNSIGNED,
-    genre TEXT,
+    artist varchar(100) NOT NULL,
+    name varchar(100) NOT NULL,
+    release_date INT NOT NULL,
+    sales DECIMAL(6, 3) NOT NULL,
+    genre VARCHAR(100),
     PRIMARY KEY (id)
 );
---
+++ albums_seeder.sql
@@ -0,0 +1,6 @@
+USE codeup_test_db;
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Douglas', 'Adams',    'Time is an illusion. Lunchtime doubly so.'),
+       ('Mark',    'Twain',    'Clothes make the man. Naked people have little or no influence on society.'),
+       ('Kurt',    'Vonnegut', 'The universe is a big place, perhaps the biggest.');
