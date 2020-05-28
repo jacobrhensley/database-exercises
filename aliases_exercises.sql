d2019f9dfb88837e5ae2ee9b1d507be69a133f68
Jacob Hensley <jacobrhensley3@gmail.com>
2020-05-28 15:45:23 -0500

    fixed spacing in aliases exercise

--- aliases_exercises.sql
+++ aliases_exercises.sql
@@ -1,5 +1,5 @@
 USE employees;
 
-SELECT concat(emp_no, '-', last_name, ',', first_name) as full_name, birth_date
+SELECT concat(emp_no, ' - ', last_name, ', ', first_name) as full_name, birth_date
 from employees
 limit 10;
